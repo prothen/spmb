e9544c7ab478c8381e0db044fda6d4bf69501706
prothen <phirot@kth.se>
2019-07-06 00:06:00 +0200

    Update spmb.h header library and include sequence

--- firmware/spmb.h
+++ firmware/spmb.h
@@ -9,7 +9,21 @@
 #include <string>
 
 #include "setup_macros.h"
-#include "cim.h"
-#include "setup.h"
-#include "util.h"
 #include "lowpass.h"
+#include "util.h"
+
+#ifdef ROS_ACTIVE
+    #ifndef ROS_LOADED
+    #define ROS_LOADED
+            #include <ros.h>
+            #include <spmbv2/request.h>
+            #include <spmbv2/actuated.h>
+            #include "ros_interface.h"
+    #endif /* ROS_LOADED */
+#endif /* ROS_ACTIVE */
+
+#include "input_rc.h"
+#include "output_i2c.h"
+#include "sm.h"
+#include "setup.h"
+
