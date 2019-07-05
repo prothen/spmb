b502de55002d0b257dc71aae564e670b1a44ff4c
prothen <phirot@kth.se>
2019-07-06 00:06:40 +0200

    Change Shell backup folder to outside of Arduino default library path

--- make_library.sh
+++ make_library.sh
@@ -1,7 +1,7 @@
 source ./../../devel/setup.bash
 cd ~/Arduino/libraries
 NAME='_backup_ros_lib_'$(date '+%Y%m%d-%H%M%S')
-cp -drf ros_lib $NAME
+cp -drf ros_lib ./../$NAME
 rm -rf ros_lib
 rosrun rosserial_arduino make_libraries.py .
 
