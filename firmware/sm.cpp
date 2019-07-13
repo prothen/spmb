f914e5b0fbc24f18db5d8ca2c862c11edd618093
prothen <phirot@kth.se>
2019-07-13 06:13:20 +0200

    Update state machine with new ros_interface and other changes

--- firmware/sm.cpp
+++ firmware/sm.cpp
@@ -1,35 +1,40 @@
 #include "spmb.h"
 
 namespace SPMB{
+  
+    StateMachine::StateMachine():
+        mControlFiltered()
+    {
+        mALIVE = true;
+        mSW_CONTROL_ACTIVE = true; // TODO: DEBUG test case - change back to false !
 
-    void StateMachine::configure(   InterruptManager* interrupt_manager_in,
-                                    ROSInterface* ros_interface_in
-                                    ){
-        // parse macros to state machine
-        //mInterruptManager = interrupt_manager_in;
-        mROSInterface = ros_interface_in;
+        mSMTimePeriod = uint16_t(s2ms * T_LOOP_RATE);
+        mCTRLTimePeriod = uint16_t(s2ms * T_CONTROL_LOOP_RATE);
 
+        mSMTimestamp = micros();
+        mCTRLTimestamp = micros();
+
+        mSWLTimePeriod = uint16_t(s2ms * SWL_TIME_PERIOD_RESET);
+        mSWLState=0;
+        mS1=false;
+        mS2=false;
+        mS1_prev=mS1;
+        mS2_prev=mS2;
     }
-
-    void StateMachine::update_signals(util::control &signals_rc){  
-        // always update interrupts
-        
-        mInterruptManager->rotate_interrupts();
-
-        // parse new time periods  
-        mInterruptManager->get_time_period("Steering", signals_rc.steering);
-        mInterruptManager->get_time_period("Velocity", signals_rc.velocity);
-        mInterruptManager->get_time_period("Transmission", signals_rc.transmission);
-        mInterruptManager->get_time_period("Differential Front", signals_rc.differential_front);
-        mInterruptManager->get_time_period("Differential Rear", signals_rc.differential_rear);
-        
-        // run logic based on tmpControl (switch mode)
+    
+    void StateMachine::configure(   InterruptManager* interrupt_manager_in, ROSInterface<myHardware>* ros_interface_in){
+        mInterruptManager = interrupt_manager_in;
+        mRosi = ros_interface_in;
     }
-
-
+    /*
+    void StateMachine::configure(   InterruptManager* interrupt_manager_in, ROSInterfaceNh* ros_interface_in){
+        mInterruptManager = interrupt_manager_in;
+        mRosi = ros_interface_in;
+    } */
+    
     void StateMachine::wait_for_next_cycle(){
-        uint16_t dt = uint16_t(micros() - timestamp);
-        if (T_loop_rate < dt){
+        uint16_t dt = us2ms * uint16_t(micros() - mSMTimestamp);
+        if (mSMTimePeriod < dt){
             util::print("Main Loop: Loop time is ", false);
             util::print(dt, false);
             util::print(" ms - no waiting for next loop required.", false);
@@ -38,34 +43,162 @@ namespace SPMB{
             util::print("Main Loop: Loop time is ", false);
             util::print(dt, false);
             util::print(" ms - wait for  ", false);
-            util::print((T_loop_rate - dt), false);
+            util::print(float(mSMTimePeriod - dt), false);
             util::print(" ms.", true);
-            delay(T_loop_rate - dt);
+            delay(mSMTimePeriod - dt);
         }
-        timestamp = micros();
+        mSMTimestamp = micros();
+    }
+    
+    void StateMachine::critical_error(){
+        util::print("Critical Erorr encountered: Nominal State Machine Operation is disable", true);
     }
 
-    void StateMachine::execute_main_loop(){
-        // add pointers to relevant modules that need to be interfaced
-            // state
-            // loop rate
-            // timestamp
-            // update timer
-            // check timer
-            // switch 
-            // define switch for different modes
-            // define callbacks for different modes
-
-
-        // define these timer placeholders as array in state machine
-        // process interrupt measurements 
-        // go through all interrupt groups and if vaild and new time period then update
-        // else toggle and reset
-        // update ros time periods 
-        // select new time periods 
-        // lowpass filetr them
-        // actuate 
-        // publish 
-        // sleep
+    void StateMachine::_update_rc_signals(util::control &signals_rc){          
+        mInterruptManager->rotate_interrupts();
+        mInterruptManager->get_time_period("Steering", signals_rc.steering);
+        mInterruptManager->get_time_period("Velocity", signals_rc.velocity);
+        mInterruptManager->get_time_period("Transmission", signals_rc.transmission);
+        mInterruptManager->get_time_period("Differential Front", signals_rc.differential_front);
+        mInterruptManager->get_time_period("Differential Rear", signals_rc.differential_rear);
+        /*
+        util::print("diff front: ", false);
+        util::print(signals_rc.differential_front, true);
+        util::print("diff rear: ", false);
+        util::print(signals_rc.differential_rear, true);
+        */
     }
+
+    void StateMachine::_swl_execute_switching_logic(util::control* signals_rc){
+        
+        if (    (mSW_CONTROL_ACTIVE && (signals_rc->velocity < PWM_CLEAR_TH_LOW)) 
+                ||!(mInterruptManager->mInterruptGroups[1]->mInterrupts.front()->mStatus)){
+            mSW_CONTROL_ACTIVE = false;
+            mSWLState = SWL_OFF;
+        }else{;}
+        
+
+        if (util::HAS_CLEAR_STATE(signals_rc->differential_front) && util::HAS_CLEAR_STATE(signals_rc->differential_rear)){
+            mS1 = util::STATE(signals_rc->differential_front);
+            mS2 = util::STATE(signals_rc->differential_rear);
+
+            /*
+            util::print("mS1: ", false);
+            util::print(mS1, true);
+            util::print("mS2: ", false);
+            util::print(mS2, true);
+            util::print("dt: ", false);
+            util::print(float(us2ms*(micros()-mSWLTimestamp)), true);
+            */
+
+            switch(mSWLState){
+                case SWL_OFF: {
+                    mSWLTimestamp = micros();
+                    if (mS1^mS2) {
+                        mSWLState = SWL_ARM;
+                        mS1_prev = mS1;
+                        mS2_prev = mS2;
+                    }else{;}
+                    break;
+                }
+                case SWL_ARM: {
+                    if ((mS1^mS2)&&(mS1!=mS1_prev)&&(mS2!=mS2_prev)) {
+                        mSWLState = SWL_SWITCH;
+                        mS1_prev = mS1;
+                        mS2_prev = mS2;
+                    }
+                    else if(!(mS1^mS2)){
+                        _swl_check_idle_transition();
+                    }
+                    else{
+                        mSWLTimestamp = micros();
+                    }
+                    break;
+                }
+                case SWL_SWITCH: {
+                    _swl_check_idle_transition();
+                    if ((mS1^mS2)&&(mS1!=mS1_prev)&&(mS2!=mS2_prev)) {
+                        mSWLState = SWL_OFF;
+                        mS1_prev = mS1;
+                        mS2_prev = mS2;
+                        mSW_CONTROL_ACTIVE = true;
+                    }else{;}
+                    break;
+                }
+                default: ; //error
+            }
+        }
+        else{;} // do nothing
+        
+        /*
+        util::print("mode:",false);
+        util::print(mSW_CONTROL_ACTIVE, true);
+        util::print("swl:",false);
+        util::print(mSWLState, true); 
+        */       
+    }
+
+    void StateMachine::_swl_check_idle_transition(){
+        if(false && util::IS_IDLE(mSWLTimestamp, mSWLTimePeriod)){ // TODO: DEBUG test case
+            mSWLState = SWL_OFF;
+            mS1 = false;
+            mS2 = false;
+            mS1_prev = mS1;
+            mS2_prev = mS2;
+            /*
+            util::print("reset state machine", true);
+            util::print("time_period: ", false);
+            util::print(mSWLTimePeriod, true);
+            util::print("time_period: ", false);
+            util::print(mSWLTimestamp, true);
+            */
+        } else{;}
+    }
+
+    void StateMachine::_process_rc_inputs(util::control &signals_rc){
+        _update_rc_signals(signals_rc);
+        _swl_execute_switching_logic(&signals_rc);
+    }
+    
+    void StateMachine::_process_ros_inputs(util::control &signals_ros){
+        mRosi->mNh.spinOnce();
+        signals_ros = mRosi->mSignals;
+    }
+
+
+    void StateMachine::update_output_signals(){
+        util::control output_signals;
+        util::control signals_rc;
+        util::control signals_ros;
+
+        _process_rc_inputs(signals_rc);
+        _process_ros_inputs(signals_ros);
+
+        if (mSW_CONTROL_ACTIVE){
+            output_signals = signals_ros;
+        }
+        else{
+            output_signals = signals_rc;
+        }
+         
+        mControlFiltered.steering.update_state(output_signals.steering);
+        mControlFiltered.velocity.update_state(output_signals.velocity);
+        mControlFiltered.transmission.update_state(output_signals.transmission);
+        mControlFiltered.differential_front.update_state(output_signals.differential_front);
+        mControlFiltered.differential_rear.update_state(output_signals.differential_rear);
+    }
+    
+    void StateMachine::_expose_actuated_signals_to_ros(){
+        
+        mRosi->publish(&mControlFiltered);
+        if (util::IS_TIME(mCTRLTimestamp, &mCTRLTimePeriod)){
+            ;//mRosi->publish(&mControlFiltered);
+        } else{;}
+    } 
+
+    void StateMachine::actuate(){
+        // TODO: add actuate either servo library or via i2c
+        _expose_actuated_signals_to_ros();
+    }
+
 }
--- firmware/sm.h
+++ firmware/sm.h
@@ -7,22 +7,52 @@ namespace SPMB{
     class StateMachine{
         public:
             InterruptManager* mInterruptManager;
-            ROSInterface* mROSInterface;
-            LowPass<uint16_t> signals(uint8_t, float);
-
-            long timestamp;
-            uint16_t time_period_main_loop; 
-            uint16_t time_period_control_loop; // TODO initialise in constructor
-            uint16_t time_period_measurement_loop;
+            ROSInterface<myHardware>* mRosi;
             
-            void configure( InterruptManager * interrupt_manager_in,
-                            ROSInterface * ros_interface_in);
 
-            void update_signals(util::control &);
+            util::control_filtered mControlFiltered;
+            bool mALIVE;
+            bool mSW_CONTROL_ACTIVE;
+            
+            long mSMTimestamp;
+            long mCTRLTimestamp;
+
+            uint16_t mSMTimePeriod; 
+            uint16_t mCTRLTimePeriod;
+            
+            // Switching Logic (SWL)
+            uint8_t mSWLState;  // 0, 1, 2 - off - arm - switch!
+            uint16_t mSWLTimePeriod;
+            long mSWLTimestamp;
+            boolean mS1;
+            boolean mS2;
+            boolean mS1_prev;
+            boolean mS2_prev;
+
+            StateMachine();
+
+            void configure( InterruptManager* interrupt_manager_in, ROSInterface<myHardware>* ros_interface_in);
+            //void configure( InterruptManager* interrupt_manager_in, ROSInterfaceNh* ros_interface_in);
+
+            void _update_rc_signals(util::control &signals_rc);
+
+            void _swl_execute_switching_logic(util::control* signals_rc);
+
+            void _swl_check_idle_transition();
 
             void wait_for_next_cycle();
             
-            void execute_main_loop();
+            void critical_error();
+
+            void _process_rc_inputs(util::control &signals_rc);
+
+            void _process_ros_inputs(util::control &signals_ros);
+
+            void update_output_signals();
+
+            void _expose_actuated_signals_to_ros();
+
+            void actuate();
     };
 
 
