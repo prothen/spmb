e1a65ee321da3d6cff9d6393624e76a6f38f1707
prothen <phirot@kth.se>
2019-07-14 23:00:17 +0200

    Update I2C template

--- firmware/output_i2c.cpp
+++ firmware/output_i2c.cpp
@@ -1,11 +1,37 @@
 #include "spmb.h"
 
 namespace SPMB{
-    OutputDriverI2C::OutputDriverI2C(util::control* signals_in){
+    OutputDriverI2C::OutputDriverI2C(util::control* signals_in):
+    driver()
+    {
         signals = signals_in;
     }
 
-    void OutputDriverI2C::actuate(){;}
+    void OutputDriverI2C::actuate(){
+/*         
+        uint16_t off_tick = 0;
+        uint8_t index = 0;
+        //convert with separate function
+        // car 1: 
+        // car 2:
+        // car 3: -32
+        // car 4: -100
+        // car 5: 
+        // car 2:
+        off_tick = uint16_t(PWM_EXTERNAL_MIN_TICK + ((ms-80-PWM_LOW)/(PWM_HIGH-PWM_LOW))*(PWM_EXTERNAL_MAX_TICK-PWM_EXTERNAL_MIN_TICK));
+        
+                //off_tick-=27;
+        if (k==0){
+                //Serial.print("External PWM "); Serial.print(k); Serial.print(" | tick"); Serial.println(off_tick);
+                //off_tick-=27;
+        }
+        else{
+        }
+        index = k*2;
+external_pwm.setPWM(index, 0 , off_tick);
+ */
+        
+        ;}
     void OutputDriverI2C::_convert_ppm_to_bits(){;}
 
 }
--- firmware/output_i2c.h
+++ firmware/output_i2c.h
@@ -4,7 +4,7 @@
 #include <Adafruit_PWMServoDriver.h>
 
 namespace SPMB{
-    
+    // PCA9685 Timer IC
     #define I2C_ADDRESS 0x00
     #define I2C_BIT_RESOLUTION 0x00
     #define I2C_BIT_LOW 
@@ -12,6 +12,8 @@ namespace SPMB{
     class OutputDriverI2C{
         public:
             OutputDriverI2C(util::control* signals);
+            
+            Adafruit_PWMServoDriver driver;
             util::control* signals; // link to state machine outputs
             void actuate();
             void _convert_ppm_to_bits();
