9967eb1766fc0700bf5bbafebe977c34ee885a8d
prothen <phirot@kth.se>
2019-07-15 13:06:32 +0200

    Add I2C output class

--- firmware/output_i2c.cpp
+++ firmware/output_i2c.cpp
@@ -1,37 +1,24 @@
 #include "spmb.h"
 
 namespace SPMB{
-    OutputDriverI2C::OutputDriverI2C(util::control* signals_in):
-    driver()
-    {
-        signals = signals_in;
+
+    void OutputDriverI2C::configure(){
+        double freq = 108.;
+        double resolution = 4096.;
+        double perus = 1000000. / freq; //us per period
+        mUsBit = perus / resolution; // us per bit
+        mOffsetDriver = 106.; // 106us offset in adafruit driver
+
+        mDriver.begin();
+        mDriver.setPWMFreq(freq);
+        delay(10);
     }
 
-    void OutputDriverI2C::actuate(){
-/*         
-        uint16_t off_tick = 0;
-        uint8_t index = 0;
-        //convert with separate function
-        // car 1: 
-        // car 2:
-        // car 3: -32
-        // car 4: -100
-        // car 5: 
-        // car 2:
-        off_tick = uint16_t(PWM_EXTERNAL_MIN_TICK + ((ms-80-PWM_LOW)/(PWM_HIGH-PWM_LOW))*(PWM_EXTERNAL_MAX_TICK-PWM_EXTERNAL_MIN_TICK));
-        
-                //off_tick-=27;
-        if (k==0){
-                //Serial.print("External PWM "); Serial.print(k); Serial.print(" | tick"); Serial.println(off_tick);
-                //off_tick-=27;
-        }
-        else{
-        }
-        index = k*2;
-external_pwm.setPWM(index, 0 , off_tick);
- */
-        
-        ;}
-    void OutputDriverI2C::_convert_ppm_to_bits(){;}
-
+    void OutputDriverI2C::actuate(util::control_filtered * output){   
+        mDriver.setPWM(M_OUT_STEERING, 0, uint16_t((output->steering.value() - mOffsetDriver) / mUsBit));
+        mDriver.setPWM(M_OUT_VELOCITY, 0, uint16_t((output->velocity.value() - mOffsetDriver) / mUsBit));
+        mDriver.setPWM(M_OUT_TRANSMISSION, 0, uint16_t((output->transmission.value() - mOffsetDriver) / mUsBit));
+        mDriver.setPWM(M_OUT_DIFFERENTIAL_FRONT, 0, uint16_t((output->differential_front.value() - mOffsetDriver) / mUsBit));
+        mDriver.setPWM(M_OUT_DIFFERENTIAL_REAR, 0, uint16_t((output->differential_rear.value() - mOffsetDriver) / mUsBit));
+    }
 }
--- firmware/output_i2c.h
+++ firmware/output_i2c.h
@@ -1,22 +1,18 @@
 #pragma once
 
 #include "spmb.h"
+
 #include <Adafruit_PWMServoDriver.h>
 
 namespace SPMB{
-    // PCA9685 Timer IC
-    #define I2C_ADDRESS 0x00
-    #define I2C_BIT_RESOLUTION 0x00
-    #define I2C_BIT_LOW 
 
     class OutputDriverI2C{
-        public:
-            OutputDriverI2C(util::control* signals);
-            
-            Adafruit_PWMServoDriver driver;
-            util::control* signals; // link to state machine outputs
-            void actuate();
-            void _convert_ppm_to_bits();
-    };
+        public:    
+            double mUsBit;
+            double mOffsetDriver;        
+            Adafruit_PWMServoDriver mDriver;
 
+            void configure();
+            void actuate(util::control_filtered* output);
+    };
 }
